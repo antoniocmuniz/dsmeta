619bbf316afb932bcf8f0d19e39124c0533cb539
Antonio Muniz <antonio.cmuniz@outlook.com>
2022-07-13 21:22:55 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import Notificationbutton from "./components/NotificationButton"
+
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <Notificationbutton />
-      <Notificationbutton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
